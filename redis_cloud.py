d135617413b52f55284c74eb656cd91f8a84f65e
BrianHammond <brianearlhammond@gmail.com>
2025-04-07 11:03:41 +0900

    Update redis_cloud.py

--- redis_cloud.py
+++ redis_cloud.py
@@ -2,9 +2,9 @@ import redis
 
 # Initialize Redis connection
 redis_cloud = redis.Redis(
-    host='redis-14416.c290.ap-northeast-1-2.ec2.redns.redis-cloud.com',
-    port=14416,
+    host='redis-cloud.com',
+    port=12345,
     decode_responses=True,
     username="default",
-    password="iRPQGAP1U7pxPRRiFoysfX4POkR0B5R4",
+    password="password",
 )
