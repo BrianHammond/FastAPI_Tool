556b7de55b34f58beb490bcae558451da80b1af6
BrianHammond <brianearlhammond@gmail.com>
2025-04-07 11:02:39 +0900

    upload

--- redis_cloud.py
+++ redis_cloud.py
@@ -2,9 +2,9 @@ import redis
 
 # Initialize Redis connection
 redis_cloud = redis.Redis(
-    host='redis-cloud.com',
-    port=12345,
+    host='redis-14416.c290.ap-northeast-1-2.ec2.redns.redis-cloud.com',
+    port=14416,
     decode_responses=True,
     username="default",
-    password="password",
+    password="iRPQGAP1U7pxPRRiFoysfX4POkR0B5R4",
 )
